530d935fe12e089885f5480b6423115d5a6e5f24
Kieran Phillips <keztricks@gmail.com>
2019-04-22 21:54:58 +0100

    Add spacegame speed increase & enemy speed multiplier

--- space-game/src/main.c
+++ space-game/src/main.c
@@ -7,6 +7,9 @@
 GameCharacter ship;
 GameCharacter bug;
 UBYTE spriteSize = 8;
+UINT8 enemyBaseSpeed = 5;
+UINT8 enemySpeedIncrease = 1;
+UINT8 enemySpeed;
 
 void moveGameCharacter(GameCharacter* character, UINT8 x, UINT8 y) {  // * is pass by ref/pointer
     move_sprite(character->spriteIDs[0], x, y);
@@ -53,14 +56,12 @@ void setupBug(){
 }
 
 void movePlayer() {
-
     if(ship.x > 8){
         if(joypad() & J_LEFT){
             ship.x -= 2;
             moveGameCharacter(&ship, ship.x, ship.y);
         }
     }
-    
     if(ship.x + ship.width <= 166){
         if(joypad() & J_RIGHT){
             ship.x += 2;
@@ -69,12 +70,23 @@ void movePlayer() {
     }
 }
 
-void main() {
+void moveEnemy() {
+        bug.y += enemySpeed;
+        moveGameCharacter(&bug, bug.x, bug.y);
 
-    printf("\n \n \n \n \n \n \n*** Press  Start ***");
-    waitpad(J_START);
-    printf("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+        if(bug.y >= 144){
+            bug.y=0;
+            bug.x = ship.x;
+            enemySpeed += enemySpeedIncrease;
+        }
+}
 
+UBYTE checkCollision(GameCharacter* firstCharacter, GameCharacter* secondCharacter) {
+    return (firstCharacter->x + 1 >= secondCharacter->x + 1 && firstCharacter->x + 1 <= secondCharacter->x + secondCharacter->width - 1) && (firstCharacter->y + 1 >= secondCharacter->y + 1 && firstCharacter->y + 1 <= secondCharacter->y + 1 + secondCharacter->height - 1) || (secondCharacter->x + 1 >= firstCharacter->x + 1 && secondCharacter->x + 1 <= firstCharacter->x + 1 + firstCharacter->width - 1) && (secondCharacter->y + 1 >= firstCharacter->y + 1 && secondCharacter->y + 1 <= firstCharacter->y + 1 + firstCharacter->height - 1);
+}
+
+void playGame() {
+    enemySpeed = enemyBaseSpeed;
     set_sprite_data(0, 8, shipBug);
     setupShip();
     setupBug();
@@ -84,9 +96,24 @@ void main() {
 
     while(TRUE) {
         movePlayer();
+        moveEnemy();
         performantDelay(5);
+
+        if (checkCollision(&ship, &bug)) {
+            printf("\n \n \n \n \n \n");
+            printf(" *** Game  Over ***\n \nHit Start to Restart\n \n \n \n \n \n \n \n \n \n");
+            waitpad(J_START);
+            printf("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            playGame();
+        }
     }
+}
+void main() {
+    //BYTE runMain = 1;
 
-    printf("\n \n \n \n \n \n \n *** Game  Over ***");
+    printf("\n \n \n \n \n \n \n*** Press  Start ***");
+    waitpad(J_START);
+    printf("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
+    playGame();
 }
